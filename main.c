ebf4786fe8067a6ba5ded721b74ce358b8f616c5
PDX PRO <pdxpro0098@gmail.com>
2025-05-18 18:20:04 +0530

    add array traversing function

--- main.c
+++ main.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
+#define ARRAY_LENGTH(arr) sizeof(arr) / sizeof(arr[0])
+
+void printArray(int array[], int len)
+{
+    for (int i = 0; i < len; i++)
+        printf("%d ", array[i]);
+}
+
 int main()
 {
     int arr[15] = {1, 12, 33, 44, 51, 67, 7, 98, 9, 0, 33, 90, 45, 13, 74};
-
-    int len = ARRAY_LENGTH(arr);
-    printArray(arr, len);
 
     return 0;
 }

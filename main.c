c781427bf6c136e32f982db46abb17280ee2e872
PDX PRO <pdxpro0098@gmail.com>
2025-05-18 18:27:12 +0530

    add reverseArray function

--- main.c
+++ main.c
@@ -9,9 +9,18 @@ void printArray(int array[], int len)
         printf("%d ", array[i]);
 }
 
+void reverseArray(int array[], int len)
+{
+    for (int i = 0; i < len / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[len - 1 - i];
+        array[len - 1 - i] = temp;
+    }
+}
+
 int main()
 {
-    int arr[15] = {1, 12, 33, 44, 51, 67, 7, 98, 9, 0, 33, 90, 45, 13, 74};
 
     return 0;
 }

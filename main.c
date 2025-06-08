37333eab953e27c69e82db1d6da2e77e083c941c
PDX PRO <pdxpro0098@gmail.com>
2025-06-08 20:27:39 +0530

    add count odd, even function

--- main.c
+++ main.c
@@ -3,27 +3,27 @@
 
 #define ARRAY_LENGTH(arr) sizeof(arr) / sizeof(arr[0])
 
-void printArray(int array[], int len)
+void printArray(int array[], int length)
 {
-    for (int i = 0; i < len; i++)
+    for (int i = 0; i < length; i++)
         printf("%d ", array[i]);
 }
 
-void reverseArray(int array[], int len)
+void reverseArray(int array[], int length)
 {
-    for (int i = 0; i < len / 2; i++)
+    for (int i = 0; i < length / 2; i++)
     {
         int temp = array[i];
-        array[i] = array[len - 1 - i];
-        array[len - 1 - i] = temp;
+        array[i] = array[length - 1 - i];
+        array[length - 1 - i] = temp;
     }
 }
 
-void sortAscendingArray(int arr[], int len)
+void sortAscendingArray(int arr[], int length)
 {
-    for (int j = 0; j < len; j++)
+    for (int j = 0; j < length; j++)
     {
-        for (int i = 0; i < len - j - 1; i++)
+        for (int i = 0; i < length - j - 1; i++)
         {
             if (arr[i] > arr[i + 1])
             {
@@ -35,11 +35,11 @@ void sortAscendingArray(int arr[], int len)
     }
 }
 
-void sortDescendingArray(int arr[], int len)
+void sortDescendingArray(int arr[], int length)
 {
-    for (int j = 0; j < len; j++)
+    for (int j = 0; j < length; j++)
     {
-        for (int i = 0; i < len - j - 1; i++)
+        for (int i = 0; i < length - j - 1; i++)
         {
             if (arr[i] < arr[i + 1])
             {
@@ -51,20 +51,41 @@ void sortDescendingArray(int arr[], int len)
     }
 }
 
-int maxArray(int array[], int len)
+void emptyArray(int array[], int length)
+{
+    memset(array, 0, length * sizeof(int));
+}
+
+void deleteElementArray(int arr[], int element, int *length)
+{
+    for (int i = 0; i < *(length); i++)
+    {
+        if (arr[i] == element)
+        {
+            for (int j = i; j < *(length)-1; j++)
+            {
+                arr[j] = arr[j + 1];
+            }
+            (*length)--;
+            break;
+        }
+    }
+}
+
+int maxArray(int array[], int length)
 {
     int max = array[0];
-    for (int i = 0; i < len; i++)
+    for (int i = 0; i < length; i++)
         if (max < array[i])
             max = array[i];
     return max;
 }
 
-int secondMaxArray(int array[], int len)
+int secondMaxArray(int array[], int length)
 {
     int max1 = array[0];
     int max2 = -2147483648;
-    for (int i = 0; i < len; i++)
+    for (int i = 0; i < length; i++)
     {
         if (max1 < array[i])
         {
@@ -79,20 +100,20 @@ int secondMaxArray(int array[], int len)
     return max2;
 }
 
-int minArray(int array[], int len)
+int minArray(int array[], int length)
 {
     int min = array[0];
-    for (int i = 0; i < len; i++)
+    for (int i = 0; i < length; i++)
         if (min > array[i])
             min = array[i];
     return min;
 }
 
-int secondMinArray(int array[], int len)
+int secondMinArray(int array[], int length)
 {
     int max1 = array[0];
     int max2 = 2147483648;
-    for (int i = 0; i < len; i++)
+    for (int i = 0; i < length; i++)
     {
         if (max1 > array[i])
         {
@@ -107,42 +128,46 @@ int secondMinArray(int array[], int len)
     return max2;
 }
 
-int sumArray(int array[], int len)
+int sumArray(int array[], int length)
 {
     int arraySum = 0;
-    for (int i = 0; i < len; i++)
+    for (int i = 0; i < length; i++)
         arraySum += array[i];
     return arraySum;
 }
 
-int productArray(int array[], int len)
+int productArray(int array[], int length)
 {
     int arrayProduct = 1;
-    for (int i = 0; i < len; i++)
+    for (int i = 0; i < length; i++)
         arrayProduct *= array[i];
     return arrayProduct;
 }
 
-void emptyArray(int array[], int len)
+int countOdd(int arr[], int length)
 {
-    memset(array, 0, len * sizeof(int));
-}
-
-
-void deleteElementArray(int arr[], int element, int *len)
-{
-    for (int i = 0; i < *(len); i++)
+    int oddCount = 0;
+    for (int i = 0; i < length; i++)
     {
-        if (arr[i] == element)
+        if (arr[i] % 2 != 0)
         {
-            for (int j = i; j < *(len)-1; j++)
-            {
-                arr[j] = arr[j + 1];
-            }
-            (*len)--;
-            break;
+            oddCount++;
         }
     }
+    return oddCount;
+}
+
+int countEven(int arr[], int length)
+{
+    int evenCount = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            evenCount++;
+        }
+    }
+    return evenCount;
 }
 
 int main()

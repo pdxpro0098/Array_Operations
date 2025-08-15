e2837384d3680d98c096012bbf20aa9b67084fdf
PDX PRO <pdxpro0098@gmail.com>
2025-08-15 10:34:56 +0530

    add sort prime

--- main.c
+++ main.c
@@ -148,7 +148,7 @@ void sortOdd(int array[], int *length)
     {
         if (array[i] % 2 != 0)
         {
-            array[j++] = array[i++];
+            array[j++] = array[i];
         }
     }
     *length = j;
@@ -174,7 +174,7 @@ void sortEven(int array[], int *length)
     {
         if (array[i] % 2 == 0)
         {
-            array[j++] = array[i++];
+            array[j++] = array[i];
         }
     }
     *length = j;
@@ -202,6 +202,31 @@ int countPrime(int arr[], int length)
         }
     }
     return length - nonPrimeCount;
+}
+
+static int isPrime(int n)
+{
+    if (n <= 1)
+        return 0;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+void sortPrime(int array[], int *length)
+{
+    int j = 0;
+    for (int i = 0; i < (*length); i++)
+    {
+        if (isPrime(array[i]))
+        {
+            array[j++] = array[i];
+        }
+    }
+    *length = j;
 }
 
 int nthMax(int arr[], int length, int nthTerm)
@@ -281,11 +306,4 @@ int popBack(int arr[], int size)
     }
     deleteElement(arr, lastElement, &size);
     return 1;
-}
-
-int main()
-{
-    int arr[5] = {1, 2, 3, 4, 5};
-
-    return 0;
 }
